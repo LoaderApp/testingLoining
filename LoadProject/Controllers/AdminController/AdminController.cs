e44af8f60464df6956adb0fcec01aab1bae63276
hamza <hamza.3738@gmail.com>
2018-04-10 17:12:01 +0500

    done
    uploadProject first time

---
+++ LoadProject/Controllers/AdminController/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoadProject.Controllers.AdminController
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminMain
+        public ActionResult AdminMain()
+        {
+            return View();
+        }
+
+
+        public ActionResult viewAvailableOrders()
+        {
+
+            return View();
+        }
+
+        public ActionResult viewAvailableQuotes()
+        {
+
+            return View();
+        }
+
+        public ActionResult viewQuoteDetails()
+        {
+
+            return View();
+        }
+        public ActionResult AcceptQuote()
+        {
+
+            return View();
+        }
+
+        public ActionResult updateOrderAsConfirmedToTransportOwner()
+        {
+
+            return View();
+        }
+
+        public ActionResult changeStatusToTransit()
+        {
+
+            return View();
+        }
+
+        public ActionResult changeStatusToCompleted()
+        {
+
+            return View();
+        }
+
+
+
+    }
+}
